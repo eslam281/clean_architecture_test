d599d01e7c32bfbc5c97b2af3b153e69a5f078f3
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-09-01 13:47:36 +0300

    get it complete

---
+++ lib/injection_container.dart
@@ -0,0 +1,30 @@
+import 'package:cleanarchitecture/features/daily_news/data/repository/article_repsoitory.dart';
+import 'package:cleanarchitecture/features/daily_news/data/sources/remote/news_api_service.dart';
+import 'package:cleanarchitecture/features/daily_news/domain/repository/article_repository.dart';
+import 'package:cleanarchitecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:cleanarchitecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async{
+  sl.registerSingleton<Dio>(
+    Dio()
+  );
+
+  sl.registerSingleton<NewsApiService>(
+    NewsApiService(sl())
+  );
+  sl.registerSingleton<ArticleRepository>(
+    ArticleRepositoryImpl(sl())
+  );
+  sl.registerSingleton<GetArticleUseCase>(
+    GetArticleUseCase(sl())
+  );
+
+  sl.registerFactory<RemoteArticleBloc>(
+      ()=> RemoteArticleBloc(sl())
+  );
+
+}
